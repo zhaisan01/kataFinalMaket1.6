f31fab86b2c5f5d17c6048fa10ed73fdc6f6ca13
zhaisan <zhaisan.moon@gamil.com>
2022-02-15 15:24:30 +0600

    Updates

--- src/js/sidebar-menu.js
+++
@@ -1,9 +0,0 @@
-const menuItem = document.querySelectorAll('.sidebar__link')
-
-for (let i = 0; i < menuItem.length; i++) {
-    menuItem[i].addEventListener("click", function() {
-        let current = document.getElementsByClassName("sidebar__link_active");
-        current[0].className = current[0].className.replace("sidebar__link_active", "");
-        this.className += " sidebar__link_active";
-    });
-}
